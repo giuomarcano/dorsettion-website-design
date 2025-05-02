265b025e8398297c8ad0f2ad9f930a0a65d03dc0
giuomarcano <eduardo.5672897@gmail.com>
2025-05-02 10:52:27 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -98,6 +98,7 @@ export default function CatalogoPage() {
                     description={product.description}
                   />
                 ))}
+                
               </div>
             </TabsContent>
 
