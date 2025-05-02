f097e0a0569b1a4277b144bad4734f172e907f0b
giuomarcano <eduardo.5672897@gmail.com>
2025-05-02 10:34:38 -0500

    Update tabs.tsx

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
-
 import { cn } from "@/lib/utils"
 
 const Tabs = TabsPrimitive.Root
@@ -14,8 +13,9 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
-      className,
+      // Aquí solo se dejan estilos base, el margen y scroll lo controlamos desde el componente padre
+      "inline-flex items-center justify-start",
+      className
     )}
     {...props}
   />
@@ -29,27 +29,14 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
-      className,
+      "whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 aria-selected:bg-gray-200 aria-selected:font-semibold transition",
+      className
     )}
     {...props}
   />
 ))
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
 
-const TabsContent = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      className,
-    )}
-    {...props}
-  />
-))
-TabsContent.displayName = TabsPrimitive.Content.displayName
+const TabsContent = TabsPrimitive.Content
 
 export { Tabs, TabsList, TabsTrigger, TabsContent }
